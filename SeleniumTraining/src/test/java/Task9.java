4e3c0380c331284d3cecc9ee83767f3fe3dbed3a
andrii.omelchuk <andrii.omelchuk@KBP1-LHP-F55921.synapse.com>
2017-04-17 13:11:14 +0300

    Finished task 9

--- SeleniumTraining/src/test/java/Task9.java
+++ SeleniumTraining/src/test/java/Task9.java
@@ -1,7 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,49 +15,81 @@ import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.google.common.io.Files;
+
 public class Task9 {
 
 	private EventFiringWebDriver driver;
 	private WebDriverWait wait;
-	
-	public static class EventsListener extends AbstractWebDriverEventListener{
-		
+
+	public static class EventsListener extends AbstractWebDriverEventListener {
+
 		@Override
-		public void beforeFindBy(By by, WebElement element, WebDriver driver){
+		public void beforeFindBy(By by, WebElement element, WebDriver driver) {
 			System.out.println(by);
 		}
-		
+
 		@Override
-		public void afterFindBy(By by, WebElement element, WebDriver driver){
+		public void afterFindBy(By by, WebElement element, WebDriver driver) {
 			System.out.println(by + " found");
 		}
-		
+
 		@Override
-		public void onException(Throwable throwable, WebDriver driver){
+		public void onException(Throwable throwable, WebDriver driver) {
 			System.out.println(throwable);
+			File tempFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			try {
+				Files.copy(tempFile, new File("screen-" + System.currentTimeMillis() + ".png"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
-	
+
 	@Before
-	public void start(){
-		
+	public void start() {
+
 		driver = new EventFiringWebDriver(new ChromeDriver());
 		driver.register(new EventsListener());
 		wait = new WebDriverWait(driver, 10);
-		
+
 	}
-	
+
 	@Test
-	public void steps(){
+	public void steps() {
+		// driver.get("http://localhost/litecart/admin/");
 		driver.get("http://google.com.ua");
-		driver.findElement(By.name("q")).sendKeys("event fired");
-		driver.findElement(By.name("btnG")).submit();
-		
+		WebElement username = driver.findElement(By.name("username"));
+		WebElement password = driver.findElement(By.name("password"));
+		username.sendKeys("admin");
+		password.sendKeys("admin");
+		driver.findElement(By.name("login")).click();
+
+		List<WebElement> menuTree = driver.findElements(By.cssSelector("li#app-"));
+
+		for (int i = 0; i < 17; i++) {
+			menuTree = driver.findElements(By.cssSelector("li#app-"));
+			menuTree.get(i).click();
+			menuTree = driver.findElements(By.cssSelector("li#app-"));
+			List<WebElement> subs = menuTree.get(i).findElements(By.cssSelector("li"));
+			if (subs.size() > 1) {
+				for (int k = 1; k < subs.size(); k++) {
+					subs.get(k).click();
+					menuTree = driver.findElements(By.cssSelector("li#app-"));
+					subs = menuTree.get(i).findElements(By.cssSelector("li"));
+				}
+			}
+
+		}
+
+		// wait.until()
+
 	}
+
 	@After
-	public void stop(){
+	public void stop() {
 		driver.quit();
 		driver = null;
 	}
-	
+
 }
