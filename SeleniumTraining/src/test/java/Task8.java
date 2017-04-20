df2504ef3a69d123f2012af701efb528e08f6d8a
andrii.omelchuk <andrii.omelchuk@KBP1-LHP-F55921.synapse.com>
2017-04-20 10:38:09 +0300

    Task8 updated

--- SeleniumTraining/src/test/java/Task8.java
+++ SeleniumTraining/src/test/java/Task8.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
+import java.util.Set;
 
 import org.junit.After;
 import org.junit.Before;
@@ -9,6 +10,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Task8 {
@@ -31,17 +34,22 @@ public class Task8 {
 		password.sendKeys("admin");
 		driver.findElement(By.name("login")).click();
 		driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
-		driver.findElement(By.cssSelector("#content > form > table > tbody > tr:nth-child(2) > td:nth-child(7) > a > i")).click();
+		driver.findElement(By.linkText("Afghanistan")).click();
 		List<WebElement> allLinks = driver.findElements(By.cssSelector("a>i.fa.fa-external-link"));
-		
-		for(int i = 0; i<7; i++){
-		allLinks.get(i).click();
-		
-		assertEquals(1+i+1,driver.getWindowHandles().size());
+		String handle = driver.getWindowHandle();
+		Set<String> newHandles;
+		for (int i = 0; i < 7; i++) {
+			allLinks.get(i).click();
+			newHandles = driver.getWindowHandles();
+			for (String a : newHandles) {
+				if (!a.equals(handle)) {
+					driver.switchTo().window(a);
+					driver.close();
+					driver.switchTo().window(handle);
+				}
+			}
 		}
 	}
-
-	
 
 	@After
 	public void stop() {
